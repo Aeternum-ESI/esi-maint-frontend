cdb2a896ca26c7e2517c4164821bf906fdb42b3b
unknown <zakaria.benhamiche@gmail.com>
2025-05-12 10:50:36 +0100

    update config

--- src/app/getToken.ts
+++ src/app/getToken.ts
@@ -1,4 +1,3 @@
-import { Auth } from "@better-fetch/fetch";
 import { cookies } from "next/headers";
 
 export async function getToken(): Promise<
@@ -12,3 +11,24 @@ export async function getToken(): Promise<
         token: (await cookies()).get("access_token")?.value,
     };
 }
+
+type Auth = Bearer | Basic | Custom;
+
+type Bearer = {
+    type: "Bearer";
+    token: typeOrTypeReturning<string | undefined>;
+};
+
+type Basic = {
+    type: "Basic";
+    username: typeOrTypeReturning<string | undefined>;
+    password: typeOrTypeReturning<string | undefined>;
+};
+
+type Custom = {
+    type: "Custom";
+    prefix: typeOrTypeReturning<string | undefined>;
+    value: typeOrTypeReturning<string | undefined>;
+};
+
+type typeOrTypeReturning<T> = T | (() => T);
